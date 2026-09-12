90b0408f74e370cd5af5b36aa12e2cc764ac4ee0
li yue <liyue@lideMac-mini.local>
2026-09-12 10:01:13 +0800

    try to fix unknown lag...

--- utils/filesystem_util.cpp
+++ utils/filesystem_util.cpp
@@ -49,9 +49,10 @@ std::string readAllLinesWithoutComment(std::string path,char prefix,std::functio
         if(line[0] == '#') {
             continue;
         }
-        if(size_t pos = line.find('#'); pos != std::string::npos) line.erase(pos);
-        if(line[0] == prefix && !handler(line)) {
-            continue;
+        
+        if(line[0] == prefix) {
+            if(size_t pos = line.find('#'); pos != std::string::npos) line.erase(pos);
+            if(!handler(line)) continue;
         }
         out.append(line);
         out.append("\n");
