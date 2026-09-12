15d4a65a41a38889d7af8f9951ec05baef59d6aa
li yue <liyue@lideMac-mini.local>
2026-09-12 09:24:22 +0800

    se/st fix

--- paradox_macro.cpp
+++ paradox_macro.cpp
@@ -228,7 +228,10 @@ void loadScriptedTrigger(std::string rootPath){
             auto [key,value] = splitWith(str,"=");
             trim(key);
             trim(value);
-            
+            size_t pos = value.find('#');
+            if(pos != std::string::npos){
+                value.erase(pos);
+            }
             simpleMacro[key] = value;
             return false;
         });
@@ -276,7 +279,10 @@ void loadScriptedEffect(std::string rootPath){
             auto [key,value] = splitWith(str,"=");
             trim(key);
             trim(value);
-            
+            size_t pos = value.find('#');
+            if(pos != std::string::npos){
+                value.erase(pos);
+            }
             simpleMacro[key] = value;
             return false;
         });
